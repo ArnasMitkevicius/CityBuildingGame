42acbf21875f7ba4977b1f45dc3633a2a4f164bc
ArnasMitkevicius <arnas.mitkevicius@stud.vgtu.lt>
2021-04-06 23:14:35 +0300

    Add project files.

---
+++ CityBuildingGame/Building.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityBuildingGame.Library
+{
+    public abstract class Building
+    {
+        public string ID { get; set; }
+        public string Title { get; set; }
+        public int Level { get; set; }
+        public Dictionary<string, double> Pricing { get; set; }
+
+        public abstract void Build(UserResourcesContainer userResourcesContainer);
+    }
+}
---
+++ CityBuildingGame/UserResourcesContainer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityBuildingGame.Library
+{
+    public class UserResourcesContainer
+    {
+        Dictionary<string, double> Resources { get; set; }
+
+        public UserResourcesContainer()
+        {
+            Resources = new Dictionary<string, double>();
+        }
+
+        public UserResourcesContainer(Dictionary<string, double> resources)
+        {
+            Resources = resources;
+        }
+
+        public void SetResources(Dictionary<string, double> resources)
+        {
+            Resources = resources;
+        }
+
+        public void AddResource(string resourceID, double quantity)
+        {
+            Resources[resourceID] += quantity;
+        }
+
+        public void RemoveResource(string resourceID, double quantity)
+        {
+            Resources[resourceID] -= quantity;
+        }
+
+    }
+}
